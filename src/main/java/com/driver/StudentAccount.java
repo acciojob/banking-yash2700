d0677f364314a3d6dee77c8c2371e175baac7d8f
BOGGALA YASWANTH REDDY <O161022@rguktong.ac.in>
2022-12-30 22:09:08 +0530

    6

--- src/main/java/com/driver/StudentAccount.java
+++ src/main/java/com/driver/StudentAccount.java
@@ -15,6 +15,7 @@ public class StudentAccount extends BankAccount{
     public StudentAccount(String name, double balance, String  institutionName) {
         //minimum balance is 0 by default
         super(name,balance,0);
+        this.institutionName=institutionName;
     }
 
 }
