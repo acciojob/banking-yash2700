ceef9dc6f6532444535b402945fdc0249ea13405
BOGGALA YASWANTH REDDY <O161022@rguktong.ac.in>
2022-12-30 22:50:59 +0530

    7

--- src/main/java/com/driver/SavingsAccount.java
+++ src/main/java/com/driver/SavingsAccount.java
@@ -18,7 +18,7 @@ public class SavingsAccount extends BankAccount{
 // 2. "Insufficient Balance" : If the amount exceeds balance
         if(amount>maxWithdrawalLimit) throw new Exception("Maximum Withdraw Limit Exceed");
         else if(amount>getBalance()) throw new Exception("Insufficient Balance");
-        else this.withdraw(amount);
+        this.withdraw(amount);
 
     }
 
