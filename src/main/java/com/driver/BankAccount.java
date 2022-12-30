82a2734cdd9b9e31d67cbc3cda7642d562628568
BOGGALA YASWANTH REDDY <O161022@rguktong.ac.in>
2022-12-30 20:55:01 +0530

    1

--- src/main/java/com/driver/BankAccount.java
+++ src/main/java/com/driver/BankAccount.java
@@ -11,21 +11,53 @@ public class BankAccount {
     }
 
     public String generateAccountNumber(int digits, int sum) throws Exception{
-        //Each digit of an account number can lie between 0 and 9 (both inclusive)
-        //Generate account number having given number of 'digits' such that the sum of digits is equal to 'sum'
-        //If it is not possible, throw "Account Number can not be generated" exception
+//Each digit of an account number can lie between 0 and 9 (both inclusive)
+//Generate account number having given number of 'digits' such that the sum of digits is equal to 'sum'
+//If it is not possible, throw "Account Number can not be generated" exception
+        int sumOfDigits=0;
+        while(digits!=0){
+            sumOfDigits+=digits%10;
+            digits/=10;
+        }
+        if(sumOfDigits!=sum) throw new Exception("Account number cannot be generated");
+        else return String.valueOf(digits);
+    }
 
-        return null;
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public double getMinBalance() {
+        return minBalance;
+    }
+
+    public void setMinBalance(double minBalance) {
+        this.minBalance = minBalance;
     }
 
     public void deposit(double amount) {
-        //add amount to balance
+//add amount to balance
+        this.balance+=amount;
 
     }
 
     public void withdraw(double amount) throws Exception {
-        // Remember to throw "Insufficient Balance" exception, if the remaining amount would be less than minimum balance
+// Remember to throw "Insufficient Balance" exception, if the remaining amount would be less than minimum balance
 
+        if (this.balance-amount<minBalance) throw new Exception("Insufficient Balance");
+        else this.balance-=amount;
     }
 
 }
--- src/main/java/com/driver/SavingsAccount.java
+++ src/main/java/com/driver/SavingsAccount.java
@@ -5,24 +5,34 @@ public class SavingsAccount extends BankAccount{
     double maxWithdrawalLimit;
 
     public SavingsAccount(String name, double balance, double maxWithdrawalLimit, double rate) {
-        // minimum balance is 0 by default
+// minimum balance is 0 by default
+        super(name,balance,0);
+        this.rate=rate;
+        this.maxWithdrawalLimit=maxWithdrawalLimit;
 
     }
     public void withdraw(double amount) throws Exception {
-        // Might throw the following errors:
-        // 1. "Maximum Withdraw Limit Exceed" : If the amount exceeds maximum withdrawal limit
-        // 2. "Insufficient Balance" : If the amount exceeds balance
+// Might throw the following errors:
+// 1. "Maximum Withdraw Limit Exceed" : If the amount exceeds maximum withdrawal limit
+// 2. "Insufficient Balance" : If the amount exceeds balance
+        if(amount>this.maxWithdrawalLimit) throw new Exception("Maximum Withdraw Limit Exceed");
+        else if(amount>this.getBalance()) throw new Exception("Insufficient Balance");
+// else{
+// double withdrawAmount=this.getBalance()-=amount;
+// this.setBalance(withdrawAmount);
+// }
 
     }
 
     public double getSimpleInterest(int years){
-        // Return the final amount considering that bank gives simple interest on current amount
+// Return the final amount considering that bank gives simple interest on current amount
+        return (this.getBalance()*this.rate*100)/100;
 
     }
 
     public double getCompoundInterest(int times, int years){
-        // Return the final amount considering that bank gives compound interest on current amount given times per year
-
+// Return the final amount considering that bank gives compound interest on current amount given times per year
+        return this.getBalance()*Math.pow(1*(this.rate/times),times*years);
     }
 
 }
