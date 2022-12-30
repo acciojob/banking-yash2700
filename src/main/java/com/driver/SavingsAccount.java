194a8c07d4ce8fada867658f0d443a588480de5e
BOGGALA YASWANTH REDDY <O161022@rguktong.ac.in>
2022-12-30 22:41:47 +0530

    7

--- src/main/java/com/driver/SavingsAccount.java
+++ src/main/java/com/driver/SavingsAccount.java
@@ -29,7 +29,7 @@ public class SavingsAccount extends BankAccount{
 
     public double getCompoundInterest(int times, int years){
 // Return the final amount considering that bank gives compound interest on current amount given times per year
-        return getBalance()*Math.pow(1+(rate/times),times*years);
+        return getBalance()*Math.pow(1+rate/(100*times),times*years);
     }
 
     public double getRate() {
