7fbb4b91a0b4fdc3430722b16e189f8456b9d7dd
BOGGALA YASWANTH REDDY <O161022@rguktong.ac.in>
2022-12-30 21:58:27 +0530

    3

--- src/main/java/com/driver/CurrentAccount.java
+++ src/main/java/com/driver/CurrentAccount.java
@@ -18,13 +18,79 @@ public class CurrentAccount extends BankAccount{
         this.tradeLicenseId=tradeLicenseId;
 
     }
+    static char getMaxCountChar(int[] count)
+    {
+        int max = 0;
+        char ch = 0;
+        for (int i = 0; i < 26; i++) {
+            if (count[i] > max) {
+                max = count[i];
+                ch = (char)((int)'a' + i);
+            }
+        }
+        return ch;
+    }
 
+    static String rearrangeString(String S)
+    {
+
+        int N = S.length();
+        if (N == 0)
+            return "";
+
+        int[] count = new int[26];
+        for (int i = 0; i < 26; i++) {
+            count[i] = 0;
+        }
+        for (char ch : S.toCharArray()) {
+            count[(int)ch - (int)'a']++;
+        }
+
+        char ch_max = getMaxCountChar(count);
+        int maxCount = count[(int)ch_max - (int)'a'];
+
+        // check if the result is possible or not
+        if (maxCount > (N + 1) / 2)
+            return "";
+
+        String res = "";
+        for (int i = 0; i < N; i++) {
+            res += ' ';
+        }
+
+        int ind = 0;
+        // filling the most frequently occurring char in the
+        // even indices
+        while (maxCount > 0) {
+            res = res.substring(0, ind) + ch_max
+                    + res.substring(ind + 1);
+            ind = ind + 2;
+            maxCount--;
+        }
+        count[(int)ch_max - (int)'a'] = 0;
+
+        // now filling the other Chars, first filling the
+        // even positions and then the odd positions
+        for (int i = 0; i < 26; i++) {
+            while (count[i] > 0) {
+                ind = (ind >= N) ? 1 : ind;
+                res = res.substring(0, ind)
+                        + (char)((int)'a' + i)
+                        + res.substring(ind + 1);
+                ind += 2;
+                count[i]--;
+            }
+        }
+        return res;
+    }
     public void validateLicenseId() throws Exception {
 // A trade license Id is said to be valid if no two consecutive characters are same
 // If the license Id is valid, do nothing
 // If the characters of the license Id can be rearranged to create any valid license Id
 // If it is not possible, throw "Valid License can not be generated" Exception
-
+        String res=CurrentAccount.rearrangeString(this.tradeLicenseId);
+        if(res==null) throw new Exception("Valid License can not be generated");
+        this.tradeLicenseId=res;
 
     }
 
