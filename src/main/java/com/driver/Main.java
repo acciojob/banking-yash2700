34e71e5d5ceceb0769fe62b087cc41e62c091694
BOGGALA YASWANTH REDDY <O161022@rguktong.ac.in>
2022-12-30 22:02:29 +0530

    5

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,7 +1,11 @@
 package com.driver;
 
 public class Main {
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws Exception {
+        BankAccount ba=new BankAccount("Yaswanth",1000,100);
+        ba.deposit(10);
+        System.out.println(ba.getBalance());
+        ba.withdraw(1000);
+        System.out.println(ba.getBalance());
     }
 }
