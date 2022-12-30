d7ecf9d7b4b76857fdb3ca9890131cd047a6c8d2
BOGGALA YASWANTH REDDY <O161022@rguktong.ac.in>
2022-12-30 21:01:21 +0530

    2

--- src/main/java/com/driver/SavingsAccount.java
+++ src/main/java/com/driver/SavingsAccount.java
@@ -35,4 +35,19 @@ public class SavingsAccount extends BankAccount{
         return this.getBalance()*Math.pow(1*(this.rate/times),times*years);
     }
 
+    public double getRate() {
+        return rate;
+    }
+
+    public void setRate(double rate) {
+        this.rate = rate;
+    }
+
+    public double getMaxWithdrawalLimit() {
+        return maxWithdrawalLimit;
+    }
+
+    public void setMaxWithdrawalLimit(double maxWithdrawalLimit) {
+        this.maxWithdrawalLimit = maxWithdrawalLimit;
+    }
 }
